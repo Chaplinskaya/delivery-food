c69b8414dee0f25a2c04e98daf9a86a0d0c93972
Tatyana <chaplinskaya.tatyana@gmail.com>
2021-09-30 12:28:25 +0300

    add dynamyc card and requests

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
         tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
-    }
+    };
     hideTabContent();
     showTabContent();//showTabContent(0) - es6 новые правила i=0 showTabContent(i=0)
 
@@ -105,7 +105,6 @@ window.addEventListener('DOMContentLoaded', () => {
         btn.addEventListener('click', openModal);
     });
     
-    
     function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show');
@@ -176,37 +175,45 @@ window.addEventListener('DOMContentLoaded', () => {
             this.parent.append(element);
         }
     }
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        ".menu .container",
-        'menu__item'
-    ).render();
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        ".menu .container",
-        'menu__item'
-    ).render();
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        12,
-        ".menu .container",
-        'menu__item',
-        'big'
-    ).render();
-//});
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
-//Forms
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);//выкинуть объект ошибки
+        }
+        return await res.json();// данные возвращаются как обычный объект, !json
+        };
+
+        getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
+
+            //NEW METHOD
+        // getResource('http://localhost:3000/menu')
+        // .then(data => createCard(data));
+
+        // function createCard(data) {
+        //     data.forEach(({img, altimg, title, descr, price}) => {
+        //         const element = document.createElement('div');
+        //         element.classList.add('menu__item');
+        //         element.innerHTML = `
+        //         <img src=${img} alt=${altimg}>
+        //         <h3 class="menu__item-subtitle">${title}</h3>
+        //         <div class="menu__item-descr">${descr}</div>
+        //         <div class="menu__item-divider"></div>
+        //         <div class="menu__item-price">
+        //             <div class="menu__item-cost">Цена:</div>
+        //             <div class="menu__item-total"><span>${price}</span> грн/день</div>
+        //         </div>
+        //         `;
+        //         document.querySelector('.menu .container').append(element);
+        //     });
+        // }
+
+        //Forms
 
 const forms = document.querySelectorAll('form');
 const message = {
@@ -215,9 +222,21 @@ const message = {
     failure: 'Что-то пошло не так'
 };
 forms.forEach(item => {
-    postData(item);
+    bindPostData(item);
 });//на каждую форму вызовет postData
-function postData(form) {
+
+const postData = async (url, data) => {
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: data
+        });
+        return await res.json();
+    };
+
+function bindPostData(form) {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         
@@ -226,41 +245,59 @@ function postData(form) {
         statusMessage.src = message.loading;
         statusMessage.style.cssText = `
             display: block;
-            margin: 0 auto;`
+            margin: 0 auto;
+        `;
 
         //statusMessage.textContent = message.loading;
         //form.appendChild(statusMessage);
+        
         form.insertAdjacentElement('afterend', statusMessage);
-        const request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
+        
+        //const request = new XMLHttpRequest();
+        //request.open('POST', 'server.php');
+        
         //request.setRequestHeader('Content-type', 'multipart/form-data');
-        request.setRequestHeader('Content-type', 'application/json, charset=utf-8');
+        //request.setRequestHeader('Content-type', 'application/json, charset=utf-8');
         
         const formData = new FormData(form);
-        const object = {};
-        formData.forEach(function(value, key) {
-            object[key] = value;
-        });
-        const json = JSON.stringify(object);
+        
+        // const object = {};
+        // formData.forEach(function(value, key) {
+        //     object[key] = value;
+        // });
 
-        //request.send(formData);
-        request.send(json);
+        const json = JSON.stringify(Object.fromEntries(formData.entries()));
+        //сначала берем formData c данными формы, превращаем в массив массивов чтобы работать с ней,
+        //превращаем в объект и затем в json и отправляем json переменную-объект на сервер
 
-        request.addEventListener('load', () => {
-            if (request.status === 200) {
-                console.log(request.response);
-                //statusMessage.textContent = message.success;
-                showThanksModal(message.success);
-                form.reset();
-                //setTimeout(() => {
-                    //statusMessage.remove();
-                //}, 2000);
-                statusMessage.remove();
-            } else {
-                //statusMessage.textContent = message.failure;
-                showThanksModal(message.failure);
-            }
+        //postData('http://localhost:3000/requests', JSON.stringify(object))
+        postData('http://localhost:3000/requests', json)
+        .then(data => {
+            console.log(data);
+            showThanksModal(message.success);
+            statusMessage.remove();
+        }).catch(() => {
+            showThanksModal(message.failure);
+        }).finally(() => {
+            form.reset();
         });
+    
+
+        //request.addEventListener('load', () => {
+        //     if (request.status === 200) {
+        //         console.log(request.response);
+        //         //statusMessage.textContent = message.success;
+        //         showThanksModal(message.success);
+        //         statusMessage.remove();
+        //         form.reset();
+        //         //setTimeout(() => {
+        //             //statusMessage.remove();
+        //         //}, 2000);
+        //     } else {
+        //         //statusMessage.textContent = message.failure;
+        //         showThanksModal(message.failure);
+        //     }
+        // });
     });
 }
 
@@ -283,6 +320,24 @@ function showThanksModal(message) {
             prevModalDialog.classList.remove('hide');
             closeModal();
         }, 4000);
-}
+};
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
 
-});
+// });
+///
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: "POST",
+//     body: JSON.stringify({name: 'Alex'}),
+//     headers: {
+//         'Content-type': 'application/json'
+//     }
+// })
+//    .then(response => response.json())
+//    .then(json => console.log(json));
+
+    fetch('http://localhost:3000/menu')// возвращается промис-обещание
+    .then(data => data.json())//превратить в обычный js объект
+    .then(res => console.log(res));
+  });
