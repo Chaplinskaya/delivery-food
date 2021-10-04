3446fd35a19e00951adc6a7c142de405f640cb16
Tatyana <chaplinskaya.tatyana@gmail.com>
2021-10-04 10:10:48 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -340,4 +340,48 @@ function showThanksModal(message) {
     fetch('http://localhost:3000/menu')// возвращается промис-обещание
     .then(data => data.json())//превратить в обычный js объект
     .then(res => console.log(res));
+
+    //slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+    prev = document.querySelector('.offer__slider-prev'),
+    next = document.querySelector('.offer__slider-next'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current');
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (сurrentdr9  < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex;
+        }
+    };
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    };
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
+
   });
