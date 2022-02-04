952ee06dd5967021e3ffefd6c72a62da10fb7405
Tatyana <chaplinskaya.tatyana@gmail.com>
2022-02-04 16:46:26 +0200

    add webpack

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-$_POST = json_decode(file_get_contents("php://input"), true);
+//$_POST = json_decode(file_get_contents("php://input"), true);
 //получить в php коде данные json и с ними поработать
 echo var_dump($_POST);
 //данные от клиента => в строку и показывает на клиенте
